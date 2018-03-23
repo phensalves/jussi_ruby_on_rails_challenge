7225ff8ef121bbdf54c41f3b9bbd252923d23b1d
RegisSeki <regisseki@gmail.com>
2018-03-23 13:35:15 -0300

    DB cofigurations and migrations

---
+++ db/migrate/20180320215713_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+  	create_table :characters, id:false do |t|
+  		t.primary_key :marvel_id
+  		t.string :name
+  		t.string :image
+  		t.datetime :modified
+
+  		t.timestamps
+  	end
+  end
+end
